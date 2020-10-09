b38a39f2dc799d0e645565da5a587a5ca52892cd
lukyjanik <luky.janik@gmail.com>
2020-10-09 16:40:47 +0200

    more complex version of parsing (only artist)

--- models.py
+++ models.py
@@ -9,6 +9,11 @@ class Track (BaseModel):
     artist: str #music.recording.artist
     tracks: str 
 
+class TrackContribution(BaseModel):
+    contributor: str
+    role: str
+    track: str
+
 # <http://rdf.freebase.com/ns/type.object.type>   <http://rdf.freebase.com/ns/music.recording>
 class Recording (BaseModel):
     artist: str # <http://rdf.freebase.com/ns/music.recording.artist> (v strede), v pravo je potom ID
@@ -34,17 +39,24 @@ class Artist(BaseModel):
     active_end: str # <http://rdf.freebase.com/ns/music.artist.active_end>    "1990"^^<http://www.w3.org/2001/XMLSchema#gYear>
     origin: str # <http://rdf.freebase.com/ns/g.119pgq6k0>        <http://rdf.freebase.com/ns/music.artist.origin>        <http://rdf.freebase.com/ns/m.049d1>
     genre: str # <http://rdf.freebase.com/ns/g.119pgq6k0>        <http://rdf.freebase.com/ns/music.artist.genre> <http://rdf.freebase.com/ns/m.05m3yc>
+    date_of_birth: str # <http://rdf.freebase.com/ns/g.12129y07> <http://rdf.freebase.com/ns/people.person.date_of_birth>        "1954-03-04"^^<http://www.w3.org/2001/XMLSchema#date>
 
     tracks = [] # <http://rdf.freebase.com/ns/music.artist.track> (v strede), vpravo je potom ID
     albums = [] # <http://rdf.freebase.com/ns/music.artist.album> (v strede), vpravo je potom ID
-    track_contributions: []
-    award_nominations: [] #<http://rdf.freebase.com/ns/g.11b6dv1czb>       <http://rdf.freebase.com/ns/award.award_nominee.award_nominations>     <http://rdf.freebase.com/ns/m.0_q_xky>
+    track_contributions = [] #<http://rdf.freebase.com/ns/g.112yfxf28>  <http://rdf.freebase.com/ns/music.artist.track_contributions>   <http://rdf.freebase.com/ns/m.01103pc7>
+    award_nominations = [] #<http://rdf.freebase.com/ns/g.11b6dv1czb>       <http://rdf.freebase.com/ns/award.award_nominee.award_nominations>     <http://rdf.freebase.com/ns/m.0_q_xky>
+    
+    music_group_members = [] # <http://rdf.freebase.com/ns/g.11b6dv1czb>       <http://rdf.freebase.com/ns/music.musical_group.member> <http://rdf.freebase.com/ns/m.0_q__qv>
 
-    person: bool # <http://rdf.freebase.com/ns/type.object.type>   <http://rdf.freebase.com/ns/people.person>
-    date_of_birth: str # <http://rdf.freebase.com/ns/g.12129y07> <http://rdf.freebase.com/ns/people.person.date_of_birth>        "1954-03-04"^^<http://www.w3.org/2001/XMLSchema#date>
+    is_person: bool = False # <http://rdf.freebase.com/ns/type.object.type>   <http://rdf.freebase.com/ns/people.person>
+    is_music_group: bool = False # <http://rdf.freebase.com/ns/g.11b6dv1czb>       <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>       <http://rdf.freebase.com/ns/music.musical_group>
+    is_music_writer: bool = False # <http://rdf.freebase.com/ns/g.12129y07> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>       <http://rdf.freebase.com/ns/music.writer>
+    is_music_composer: bool = False # <http://rdf.freebase.com/ns/g.12129y07> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>       <http://rdf.freebase.com/ns/music.composer>
 
 
-    is_music_writer: bool # <http://rdf.freebase.com/ns/g.12129y07> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>       <http://rdf.freebase.com/ns/music.writer>
-    is_music_composer: bool # <http://rdf.freebase.com/ns/g.12129y07> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>       <http://rdf.freebase.com/ns/music.composer>
 
+# \<http:\/\/rdf\.freebase\.com\/ns\/m\.0v1_lyv\>
 
+# m\.014brgq
+
+# <http://rdf.freebase.com/ns/m.01czfhb>       <http://rdf.freebase.com/ns/music.artist.track> <http://rdf.freebase.com/ns/g.11b6dv1czb>
--- parsing.py
+++ parsing.py
@@ -1,32 +1,51 @@
-from os import listdir
+from os import listdir, system
 import gzip
 import json
 import re
 
-from models import Artist, Album, Track
+from models import Artist, Album, Track, TrackContribution
 
 file = 'freebase-head-1000000'
+
+clear = lambda: system('cls')
 
 # path_to_file = '../data'
 # with gzip.GzipFile(file, 'r') as fin:   
 #     for line in fin:
 #         print(line)
 ARTIST = 'artist'
+TRACK = 'track'
+ALBUM = 'album'
+TRACK_CONTRIBUTION = 'track_contribution'
+
+ENTITY_CREATION_BY_NAME = {
+    'artist': Artist,
+    'track': Track,
+    'album': Album,
+    'track_contribution': TrackContribution
+}
+
 # Matching patterns
-PATTERN_ARTIST = r'\<.*\>\s+(\<.*ns#type\>|\<.*type\.object\.type\>)\s+\<.*ns\/music\.artist\>'
+# PATTERN_ARTIST = r'\<.*\>\s+(\<.*ns#type\>|\<.*type\.object\.type\>)\s+\<.*ns\/music\.artist\>'
+PATTERN_ARTIST = r'\<.*\>\s+(\<.*(ns#type|type\.object\.type)\>\s+\<.*ns\/music\.artist\>|\<.*music\.artist.*\>\s+\<.*\>)'
 PATTERN_ARTIST_TRACK = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.track\>\t+\<.*\>'
+PATTERN_ARTIST_TRACK_CONTRIBUTION = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.track_contributions\>\t+\<.*\>'
 PATTERN_ARTIST_ALBUM = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.album\>\t+\<.*\>'
 PATTERN_ARTIST_ACTIVE_START = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.active_start\>\t+\<.*\>'
 PATTERN_ARTIST_ACTIVE_END = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.active_end\>\t+\<.*\>'
+PATTERN_ARTIST_ORIGIN = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.origin\>\t+\<.*\>'
+PATTERN_ARTIST_GENRE = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.genre\>\t+\<.*\>'
 PATTERN_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/people\.person\.date_of_birth\>\t+\<.*\>'
+PATTERN_ARTIST_NAME = r'\<.*\>\t+\<http:\/\/rdf\.freebase\.com\/ns\/type\.object\.name\>\t+\".*\"@en'
 
 # Value retrieval patterns
 PATTERN_RETRIEVE_ARTIST_ACTIVE_DATE = r'(?<!not )((?<=\t\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.active_end\>\t\")|(?<=\t\<http:\/\/rdf\.freebase\.com\/ns\/music\.artist\.active_start\>\t\"))([0-9-]*)'
 PATTERN_RETRIEVE_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH = r'(?<!not )(?<=\t\<http:\/\/rdf\.freebase\.com\/ns\/people\.person\.date_of_birth\>\t\")([0-9-]*)'
+PATTERN_RETRIEVE_ARTIST_NAME = r'(?<!not )(?<=\t\<http:\/\/rdf\.freebase\.com\/ns\/type\.object\.name\>\t\")([\sa-zA-z]*)'
 
 # Value retrieval patterns
-PATTERN_SUBJECT_ID = r'(?<=\<http:\/\/rdf\.freebase\.com\/ns\/)(g\.[a-z0-9_]+)'
-PATTERN_OBJECT_ID = r'(?<!not )(?<=\t\<http:\/\/rdf\.freebase\.com\/ns\/)[a-z]\.[a-z0-9]+'
+PATTERN_SUBJECT_ID = r'(?<=\<http:\/\/rdf\.freebase\.com\/ns\/)([a-z]\.[a-z0-9_]+)'
+PATTERN_OBJECT_ID = r'(?<!not )(?<=\t\<http:\/\/rdf\.freebase\.com\/ns\/)[a-z]\.[a-z0-9_]+'
 
 def getSubjectId(input: str):
     result = re.search(PATTERN_SUBJECT_ID, line)
@@ -38,18 +57,70 @@ def getObjectId(input: str):
 
 def getDate(pattern: any, input: str):
     result = re.search(pattern, line)
-    return result.group(0) 
+    return result.group(0)
+
+def getName(input: str):
+    result = re.search(PATTERN_RETRIEVE_ARTIST_NAME, line)
+    return result.group(0)
+
+def getOrCreate(data: dict, entity: str, id: int):
+    entity_instance = None
+    if (id not in data[entity]):
+        entity_instance = ENTITY_CREATION_BY_NAME[entity](id)
+        data[entity][id] = entity_instance
+    else:
+        entity_instance = data[entity][id]
+    return entity_instance, data
 
 f = open(file, "r", encoding="utf8")
 line = f.readline()
 
 found_id = None
 currently_parsing = None
+line_counter: int = 0
 
 data = dict()
-data['artists'] = dict()
-data['tracks'] = dict()
-data['albums'] = dict()
+data['artist'] = dict()
+data['track'] = dict()
+data['album'] = dict()
+data['track_contribution'] = dict()
+
+data['recording'] = dict()
+
+def handleArtistParsing(data, line: str):
+    if (re.match(PATTERN_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH, line)):
+        data['artist'][found_id].date_of_birth = getDate(PATTERN_RETRIEVE_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH, line)
+    elif (re.match(PATTERN_ARTIST_ACTIVE_START, line) or re.match(PATTERN_ARTIST_ACTIVE_END, line)):
+        data['artist'][found_id]['active_start' if re.match(PATTERN_ARTIST_ACTIVE_START, line) else 'active_end'] = getDate(PATTERN_RETRIEVE_ARTIST_ACTIVE_DATE,line)                
+    elif (re.match(PATTERN_ARTIST_ORIGIN, line)):
+        data['artist'][found_id].origin = getObjectId(line)
+    elif (re.match(PATTERN_ARTIST_GENRE, line)):
+        data['artist'][found_id].genre = getObjectId(line)
+    elif (re.match(PATTERN_ARTIST_NAME, line)):
+        data['artist'][found_id].name = getName(line)
+    elif (re.match(PATTERN_ARTIST_TRACK, line)):
+        track_id = getObjectId(line)
+        track, data = getOrCreate(data, TRACK, track_id)
+        data['artist'][found_id].tracks.append(track)
+    elif (re.match(PATTERN_ARTIST_ALBUM, line)):
+        album_id = getObjectId(line)
+        album, data = getOrCreate(data, ALBUM, album_id)
+        data['artist'][found_id].albums.append(album)
+    elif (re.match(PATTERN_ARTIST_ALBUM, line)):
+        album_id = getObjectId(line)
+        album, data = getOrCreate(data, ALBUM, album_id)
+        data['artist'][found_id].albums.append(album)
+    elif (re.match(PATTERN_ARTIST_TRACK_CONTRIBUTION, line)):
+        contribution_id = getObjectId(line)
+        track_contribution, data = getOrCreate(data, TRACK_CONTRIBUTION, contribution_id)
+        data['artist'][found_id].track_contributions.append(track_contribution)
+    return data
+
+
+ENTITY_PARSING_BY_NAME = {
+    'artist': handleArtistParsing,
+}
+
 
 
 while (line != None):
@@ -58,26 +129,42 @@ while (line != None):
         if (re.match(PATTERN_ARTIST, line)):
             currently_parsing = ARTIST
             found_id = getSubjectId(line)
-            data['artists'][found_id] = Artist(found_id)
+            artist, data = getOrCreate(data, ARTIST, found_id)
         else:
             found_id = None
             currently_parsing = None
-    else:
-        if (currently_parsing == ARTIST):
-            if (re.match(PATTERN_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH, line)):
-                data['artists'][found_id].date_of_birth = getDate(PATTERN_RETRIEVE_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH, line)
-            elif (re.match(PATTERN_ARTIST_ACTIVE_START, line) or re.match(PATTERN_ARTIST_ACTIVE_END, line)):
-                data['artists'][found_id]['active_start' if re.match(PATTERN_ARTIST_ACTIVE_START, line) else 'active_end'] = getDate(PATTERN_RETRIEVE_ARTIST_ACTIVE_DATE,line)                
-            elif (re.match(PATTERN_ARTIST_TRACK, line)):
-                track_id = getObjectId(line)
-                if (track_id not in data['tracks']):
-                    new_track = Track(track_id)
-                    data['tracks'][track_id] = new_track
-                    data['artists'][found_id].tracks.append(new_track)
-            elif (re.match(PATTERN_ARTIST_ALBUM, line)):
-                album_id = getObjectId(line)
-                if (album_id not in data['albums']):
-                    new_album = Album(album_id)
-                    data['albums'][album_id] = new_album
-                    data['artists'][found_id].albums.append(new_track)
+    if (currently_parsing != None):
+        data = ENTITY_PARSING_BY_NAME[currently_parsing](data, line)
+    # if (currently_parsing == ARTIST):
+    #     data = handleArtistParsing(data, line)
+        # if (re.match(PATTERN_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH, line)):
+        #     data['artist'][found_id].date_of_birth = getDate(PATTERN_RETRIEVE_ARTIST_PEOPLE_PERSON_DATE_OF_BIRTH, line)
+        # elif (re.match(PATTERN_ARTIST_ACTIVE_START, line) or re.match(PATTERN_ARTIST_ACTIVE_END, line)):
+        #     data['artist'][found_id]['active_start' if re.match(PATTERN_ARTIST_ACTIVE_START, line) else 'active_end'] = getDate(PATTERN_RETRIEVE_ARTIST_ACTIVE_DATE,line)                
+        # elif (re.match(PATTERN_ARTIST_ORIGIN, line)):
+        #     data['artist'][found_id].origin = getObjectId(line)
+        # elif (re.match(PATTERN_ARTIST_GENRE, line)):
+        #     data['artist'][found_id].genre = getObjectId(line)
+        # elif (re.match(PATTERN_ARTIST_NAME, line)):
+        #     data['artist'][found_id].name = getName(line)
+        # elif (re.match(PATTERN_ARTIST_TRACK, line)):
+        #     track_id = getObjectId(line)
+        #     track, data = getOrCreate(data, TRACK, track_id)
+        #     data['artist'][found_id].tracks.append(track)
+        # elif (re.match(PATTERN_ARTIST_ALBUM, line)):
+        #     album_id = getObjectId(line)
+        #     album, data = getOrCreate(data, ALBUM, album_id)
+        #     data['artist'][found_id].albums.append(album)
+        # elif (re.match(PATTERN_ARTIST_ALBUM, line)):
+        #     album_id = getObjectId(line)
+        #     album, data = getOrCreate(data, ALBUM, album_id)
+        #     data['artist'][found_id].albums.append(album)
+        # elif (re.match(PATTERN_ARTIST_TRACK_CONTRIBUTION, line)):
+        #     contribution_id = getObjectId(line)
+        #     track_contribution, data = getOrCreate(data, TRACK_CONTRIBUTION, contribution_id)
+        #     data['artist'][found_id].track_contributions.append(track_contribution)
+    if (line_counter % 100000 == 0):
+        clear()
     line = f.readline()
+    line_counter += 1
+    print(line_counter)
