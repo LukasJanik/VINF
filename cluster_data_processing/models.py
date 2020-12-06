a574602444acb1ec487dae3c82c61fb7517f380f
Lukáš Janík <lukas.janik@bootiq.io>
2020-12-06 12:48:12 +0100

    added final version of cluster data processing

--- cluster_data_processing/models.py
+++ cluster_data_processing/models.py
@@ -1,25 +1,40 @@
+def processDescription(description:str) -> str:
+    return description.replace('\n', '').replace('\"', '"') if description != None else None
+
+def transformDate(date:str) -> str:
+    if date == None:
+        return None
+
+    date_parts = date.strip().split('-')
+    
+    no_of_parts = len(date_parts)
+
+    if no_of_parts == 3:
+        return date
+    if no_of_parts == 2:
+        return f"{date}-01"
+    else:
+        return f"{date}-01-01"
+
 class Track:
     name: str
-    description: str
-    length = int                
+    description: str          
 
-    def __init__(self, id:int, name:str, description:str, length:int):
+    def __init__(self, id:str, name:str, description:str):
         self.id = id
         self.name = name
-        self.description = description
-        self.length = length if length != '' else None
+        self.description = processDescription(description)
     
 class Album:
     name: str
     description: str        
+    release_date: int       
 
-    release_date: str       
-
-    def __init__(self, id:int, name:str, description:str, release_date:str):
+    def __init__(self, id:str, name:str, description:str, release_date:str):
         self.id = id
         self.name = name
-        self.description = description
-        self.release_date = release_date
+        self.description = processDescription(description)
+        self.release_date = transformDate(release_date)
 
 class Artist:
     name: str
@@ -30,36 +45,35 @@ class Artist:
 
     tracks: []                  
     albums: []                  
-    awards_won: []              
+    awards_won: []
 
-    def __init__(self, name:str, description:str, active_start:str, active_end:str, date_of_birth:str):
+    no_of_tracks = 0
+    no_of_albums = 0
+    no_of_awards_won = 0              
+
+    def __init__(self, id:str, name:str, description:str, active_start:str, active_end:str, date_of_birth:str):
         self.id = id
         self.name = name
-        self.description = description
-        self.active_start = active_start
-        self.active_end = active_end
-        self.date_of_birth = date_of_birth
+        self.description = processDescription(description)
+        self.active_start = transformDate(active_start)
+        self.active_end = transformDate(active_end)
+        self.date_of_birth = transformDate(date_of_birth)
 
         self.tracks = []
         self.albums = []
         self.awards_won = []
+
+        self.no_of_tracks = 0
+        self.no_of_albums = 0
+        self.no_of_awards_won = 0 
+        
 
 class Award:
     id: int
     name: str
     description: str
 
-    def __init__(self, id: int, name:str, description:str):
+    def __init__(self, id:str, name:str, description:str):
         self.id = id
         self.name = name
-        self.description = description
-    
-# class Genre:
-#     id: int
-#     name: str                 
-#     genre_type: str         
-    
-#     def __init__(self, id:int, name:str, genre_type:str):
-#         self.id = id
-#         self.name = name 
-#         self.genre_type = genre_type 
+        self.description = processDescription(description)
--- cluster_data_processing/normalize_data.py
+++ cluster_data_processing/normalize_data.py
@@ -1,6 +1,7 @@
 import jsonpickle
 import json
 import sys
+import numpy as np
 from models import *
 
 ARTIST = 'artist'
@@ -8,6 +9,26 @@ TRACK = 'track'
 ALBUM = 'album'
 GENRE = 'genre'
 AWARD = 'award_honor'
+
+def max_statistics(statistics, length, name, id):
+    statistics['max_value'] = length
+    statistics['artists'].append({
+        'id': id,
+        'name': name,
+        'value': length
+        })
+    statistics['artists'] = statistics['artists'][-5:]
+    return statistics
+
+def getStatistics(data):
+    return {
+        'with_0': len(data) - np.count_nonzero(data),
+        'max:': max(data),
+        'mean': np.mean(data),
+        'min': min(data),
+        'med': np.median(data)
+    }
+
 
 def loadArtists():
     with open(f"./filtered_output/artist/artist_0.json", "r") as file:
@@ -35,6 +56,9 @@ def loadTracks(id:int):
         return json.load(file)
 
 def createFinalArtists(artists_tmp):
+
+    statistics['artists']['total'] = len(artists_tmp)
+
     artists_final = dict()
 
     album_to_artist = dict()
@@ -53,6 +77,7 @@ def createFinalArtists(artists_tmp):
         artist_id = tmp_artist["id"]
 
         artists_final[artist_id] = Artist(
+            artist_id,
             tmp_artist['name'],
             tmp_artist['description'],
             tmp_artist['active_start'],
@@ -77,26 +102,14 @@ def createFinalArtists(artists_tmp):
                 award_to_artist[award] = [artist_id]
             else:
                 award_to_artist[award].append(artist_id)
-        # if (artist_genre != None):    
-        #     if (artist_genre not in genre_to_artist):
-        #         genre_to_artist[artist_genre] = [artist_id]
-        #     else:
-        #         genre_to_artist[artist_genre].append(artist_id)
 
     return artists_final, album_to_artist, track_to_artist, award_to_artist, genre_to_artist
 
-# def fillGenre(artists_final: dict, genre_to_artist: dict):
-#     genres = loadGenres()
-    
-#     for genre in genres:
-#         genre_id = genres[genre]['id']
-#         if (genre_id in genre_to_artist):
-#             for artist in genre_to_artist[genre_id]:
-#                 artists_final[artist].genre = genres[genre]['name']
-
 def fillAlbums(artists_final, album_to_artist):
+    print('Loading Albums')
     albums = loadAlbums()
-
+    statistics['albums']['total'] = len(albums)
+    print('Filling albums')
     for album in albums:
         album_tmp = albums[album]
         album_final = Album(
@@ -111,11 +124,23 @@ def fillAlbums(artists_final, album_to_artist):
             artist_list = set(artist_list + album_to_artist[album_tmp['id']])
             
         for artist in artist_list:
-            artists_final[artist].albums.append(album_final)
+            if (artist in artists_final):
+                artists_final[artist].albums.append(album_final)
+
+                statistics['albums']['name'].append(0 if album_final.name == None else len(album_final.name))
+                statistics['albums']['description'].append(0 if album_final.description == None else len(album_final.description))
+
+                if (album_final.release_date == None):
+                    statistics['albums']['without_release_date'] += 1
+
+            else:
+                statistics['albums']['without_artist'] += 1
 
 def fillAwards(artists_final, award_to_artist):
+    print('Loading Awards')
     awards = loadAwards()
-
+    statistics['awards']['total'] = len(awards)
+    print('Filling Awards')
     for award in awards:
         award_tmp = awards[award]
         award_final = Award(
@@ -127,20 +152,27 @@ def fillAwards(artists_final, award_to_artist):
         if (award_tmp['id'] in award_to_artist):
             artist_list = award_to_artist[award_tmp['id']]
             for artist in artist_list:
-                artists_final[artist].awards_won.append(award_final)
+                if(artist in artists_final):
+                    artists_final[artist].awards_won.append(award_final)
+
+                    statistics['awards']['name'].append(0 if award_final.name == None else len(award_final.name))
+                    statistics['awards']['description'].append(0 if award_final.description == None else len(award_final.description))
+
+                else:
+                    statistics['awards']['without_artist'] += 1
 
 def fillTracks(artists_final, track_to_artist, number_of_track_bulks):
-    count = 0
     for track_bulk in range(number_of_track_bulks):
+        print(f"Loading Tracks {track_bulk}")
         tracks = loadTracks(track_bulk)
-
+        statistics['tracks']['total'] += len(tracks)
+        print(f"Filling Tracks {track_bulk}")
         for track in tracks:
             track_tmp = tracks[track]
             track_final = Track(
                 track_tmp['id'],
                 track_tmp['name'],
-                track_tmp['description'],
-                track_tmp['length'],
+                track_tmp['description']
             )
             
             artist_list = track_tmp['artists']
@@ -150,40 +182,155 @@ def fillTracks(artists_final, track_to_artist, number_of_track_bulks):
             for artist in artist_list:
                 if (artist in artists_final):
                     artists_final[artist].tracks.append(track_final)
+
+                    statistics['tracks']['name'].append(0 if track_final.name == None else len(track_final.name))
+                    statistics['tracks']['description'].append(0 if track_final.description == None else len(track_final.description))
+
                 else:
-                    count += 1
-    print(f"Tracks without artists: {count}")
+                    statistics['tracks']['without_artist'] += 1
 
 def writeFinalArtists(artists_final):
     count = 0
     file = None
     file_index = 0
     
-    # length = len(artists_final)
-    # number_of_files = length / 50000 if length % 50000 == 0 else (length / 50000) + 1
-    
-    # for file_index in range(number_of_files):
-    #     with open(f"./final_output/artist_{file_index}.json", "w") as file:
-    #         return json.load(file)
-    
+    print('Saving artists')
     for artist in artists_final:
         if (count == 0 or count % 50000 == 0):
             if (file != None):
                 file.close()
             file = open(f"./final_output/artist_{file_index}.json", "w")
             file_index += 1
-        file.write(f"{jsonpickle.encode(artists_final[artist])}\n", unpicklable=False)
+        
+        tracks_length = len(artists_final[artist].tracks)
+        albums_length = len(artists_final[artist].albums)
+        awards_won_length = len(artists_final[artist].awards_won)
+
+        # Statistics
+
+        artists_final[artist].no_of_tracks = tracks_length 
+        statistics['artists']['tracks'].append(tracks_length)
+
+        if (tracks_length >= statistics['artists']['max_tracks']['max_value']):
+            statistics['artists']['max_tracks'] = max_statistics(
+                statistics['artists']['max_tracks'],
+                tracks_length,
+                artists_final[artist].name,
+                artists_final[artist].id
+            )
+
+        artists_final[artist].no_of_albums = albums_length
+        statistics['artists']['albums'].append(albums_length)
+
+        if (albums_length >= statistics['artists']['max_albums']['max_value']):
+            statistics['artists']['max_albums'] = max_statistics(
+                statistics['artists']['max_albums'],
+                tracks_length,
+                artists_final[artist].name,
+                artists_final[artist].id
+            )
+
+        artists_final[artist].no_of_awards_won = awards_won_length
+        statistics['artists']['awards'].append(awards_won_length)
+
+        if (awards_won_length >= statistics['artists']['max_awards']['max_value']):
+            statistics['artists']['max_awards'] = max_statistics(
+                statistics['artists']['max_awards'],
+                tracks_length,
+                artists_final[artist].name,
+                artists_final[artist].id
+            )
+        
+        statistics['artists']['name'].append(0 if artists_final[artist].name == None else len(artists_final[artist].name))
+        
+        statistics['artists']['description'].append(0 if artists_final[artist].description == None else len(artists_final[artist].description))
+
+        if (artists_final[artist].active_start == None):
+            statistics['artists']['without_active_start'] += 1
+        
+        if (artists_final[artist].active_end == None):
+            statistics['artists']['without_active_end'] += 1
+
+        if (artists_final[artist].date_of_birth == None):
+            statistics['artists']['without_date_of_birth'] += 1
+
+        file.write(f"{jsonpickle.encode(artists_final[artist], unpicklable=False)}\n")
         count += 1
     if (file != None and not file.closed):
         file.close()
 
+statistics = {
+    'artists': {
+        'name': [], #
+        'description': [], #
+        'without_active_start': 0, #
+        'without_active_end': 0, #
+        'without_date_of_birth': 0, #
+        'tracks': [], 
+        'awards': [], 
+        'albums': [], 
+        'max_tracks': {  #
+            'max_value': 0,
+            'artists': []
+        },
+        'max_albums': { #
+            'max_value': 0,
+            'artists': []
+        },
+        'max_awards': { #
+            'max_value': 0,
+            'artists': []
+        },
+        'total': 0
+    },
+    'tracks': {
+        'without_artist': 0, 
+        'name': [], #
+        'description': [], #
+        'total': 0
+    },
+    'albums': {
+        'name': [], #
+        'description': [], #
+        'without_artist': 0, # 
+        'without_release_date': 0,
+        'total': 0
+    },    
+    'awards': {
+        'name': [], #
+        'description': [], #
+        'without_artist': 0, # 
+        'total': 0
+    }
+}
+
 artists_tmp = loadArtists()
 artists_final, album_to_artist, track_to_artist, award_to_artist, genre_to_artist = createFinalArtists(artists_tmp)
 
-fillGenre(artists_final, genre_to_artist)
-# fillAlbums(artists_final, album_to_artist)
-# fillAwards(artists_final, award_to_artist)
-# fillTracks(artists_final, track_to_artist, 11)
+# fillGenre(artists_final, genre_to_artist)
+fillAlbums(artists_final, album_to_artist)
+fillAwards(artists_final, award_to_artist)
+fillTracks(artists_final, track_to_artist, 11)
 
-# writeFinalArtists(artists_final)
+writeFinalArtists(artists_final)
 
+statistics['artists']['tracks'] = getStatistics(statistics['artists']['tracks'])
+statistics['artists']['awards'] = getStatistics(statistics['artists']['awards'])
+statistics['artists']['albums'] = getStatistics(statistics['artists']['albums'])
+
+statistics['artists']['name'] = getStatistics(statistics['artists']['name'])
+statistics['artists']['description'] = getStatistics(statistics['artists']['description'])
+
+statistics['tracks']['name'] = getStatistics(statistics['tracks']['name'])
+statistics['tracks']['description'] = getStatistics(statistics['tracks']['description'])
+
+statistics['albums']['name'] = getStatistics(statistics['albums']['name'])
+statistics['albums']['description'] = getStatistics(statistics['albums']['description'])
+
+statistics['awards']['name'] = getStatistics(statistics['awards']['name'])
+statistics['awards']['description'] = getStatistics(statistics['awards']['description'])
+
+
+file = open('statistics.json', 'w')
+file.write(jsonpickle.encode(statistics, unpicklable=False))
+file.close()
